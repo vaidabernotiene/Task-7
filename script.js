87cdb14f4c52c25c6d2922e23a7c045ce3553ae2
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-02-20 13:04:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objektą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
